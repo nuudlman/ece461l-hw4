5c9e110a52995f93212b99fe84506bb777a95be0
Isaac Nudelman <isaac.nudelman@utexas.edu>
2024-10-12 19:04:56 -0500

    Center things

--- src/components/HardwareSet.tsx
+++ src/components/HardwareSet.tsx
@@ -1,4 +1,4 @@
-import { Button, Box, Paper, Stack, TextField } from "@mui/material";
+import { Button, Paper, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 
 export default function HardwareSet(props: {id: number, capacity: number}) {
@@ -7,10 +7,11 @@ export default function HardwareSet(props: {id: number, capacity: number}) {
         <>
             <Paper>
                 <Stack direction="row" spacing={5} padding={1}>
-                    <Box>
-                        <strong>HWSet{props.id}</strong>: {available}/{props.capacity}
-                    </Box>
-                    <TextField label="Quantity"/>
+                    <Stack sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <strong>HWSet{props.id}</strong>
+                        <div>{available}/{props.capacity}</div> 
+                    </Stack>
+                    <TextField label="Quantity" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}/>
                     <Button variant="contained">Check In</Button>
                     <Button variant="contained">Check Out</Button>
                 </Stack>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,23 +1,37 @@
 import { Paper, Box, Stack, Button } from "@mui/material";
+import { useState } from "react";
 import HardwareSet from "./HardwareSet";
 
+
 export default function Project(props: {name: string, users?: string[], hardware: number[]}) {
-    let users;
-    if (props.users != undefined)
-        users = <ul>{props.users.map((user) => (<li>{user}</li>))}</ul>;
-    else
-        users = "No Users";
+    const [users, setUsers] = useState(props.users == undefined ? [] : props.users)
+    const [joined, setJoined] = useState(false);
+
+    const renderUsers = () => {
+        if (users.length < 1)
+            return "No Users";
+
+        return <ul>{users.map((user) => (<li>{user}</li>))}</ul>;
+    };
 
     return (
         <>
             <Paper>
                 <Stack direction="row" spacing={2} padding={2}>
-                    <Stack padding={2} maxWidth='sm'>
+                    <Stack padding={2} maxWidth='100%' width='250px'>
                         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}><h3>{props.name}</h3></Box>
-                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>{users}</Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>{renderUsers()}</Box>
                     </Stack>
                     <Stack spacing={2}>{props.hardware.map((val, idx) => (<HardwareSet id={idx} capacity={val} />))}</Stack>
-                    <Button variant="contained">Join</Button>
+                    <Button variant="contained" onClick={() => {
+                        if (!joined){
+                            setUsers(prevUsers => [...prevUsers, navigator.userAgent]);
+                            setJoined(true);
+                        } else {
+                            setUsers(prevUsers => prevUsers.filter((user) => user !== navigator.userAgent));
+                            setJoined(false);
+                        }
+                    }}>{joined ? "Leave" : "Join"}</Button>
                 </Stack>
             </Paper>
         </>
